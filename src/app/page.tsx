653dd42d564bd744866ebe5c097424e08e6a05a4
Centeno448 <diegocenteno448@gmail.com>
2025-01-06 17:27:43 -0500

    initial page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,52 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className={styles.img}
+          src="/img/me.jpg"
+          alt="Photo of Diego Centeno"
+          width={400}
+          height={400}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.intro}>
+          <p>
+            Hello there! I&apos;m Diego Centeno, a fullstack software developer
+            interested in game development and multiplayer server architecture.
+          </p>
+          <div className={styles.ctas}>
+            <Link className={styles.primary} href="/projects">
+              Check out my projects
+            </Link>
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
+          href="https://www.linkedin.com/in/diego-andres-centeno/"
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className={styles.socialLogo}
+            src="/linked-in.svg"
+            alt="LinkedIn logo"
+            width={30}
+            height={30}
           />
-          Learn
         </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://github.com/Centeno448/" rel="noopener noreferrer">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            className={styles.socialLogo}
+            src="/github-mark.svg"
+            alt="Github logo"
+            width={30}
+            height={30}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
