008298995a3dc8578a7aa070d62fa22acfb8cf06
Centeno448 <diegocenteno448@gmail.com>
2025-01-07 18:26:37 -0500

    add project container to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,17 @@
 import { fetchCMSContent } from "../utils";
 import { ListProject } from "../types";
 import Project from "./project";
+import styles from "./page.module.scss";
 
 export default async function Projects() {
   const data = await fetchCMSContent("api/projects");
   const projects = await data.json();
 
   return (
-    <>
+    <div className={styles.projectContainer}>
       {projects.data.map((project: ListProject) => (
         <Project key={project.documentId} {...project} />
       ))}
-    </>
+    </div>
   );
 }
