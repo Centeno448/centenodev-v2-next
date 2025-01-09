384d11bf7ebaf24e1b22edc9dc58829a03807dca
Centeno448 <diegocenteno448@gmail.com>
2025-01-08 19:00:21 -0500

    add not found route

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "./not-found.module.scss";
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>404 - Not Found</h2>
+      <div className={styles.ctas}>
+        <Link className={styles.primary} href="/">
+          Return Home
+        </Link>
+      </div>
+    </div>
+  );
+}
